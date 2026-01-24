28d87f1054e3128c903c5cd663201a6325fd67cc
Adarsh Jain <adarsh.jain428@gmail.com>
2026-01-24 16:53:14 +0530

    Docker & Docker compose update

--- backend/server.js
+++ backend/server.js
@@ -48,6 +48,8 @@ if (process.env.NODE_ENV === 'production') {
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, () =>
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
-);
+app.listen(port, '0.0.0.0', () => {
+  console.log(
+    `Server running in ${process.env.NODE_ENV || 'development'} mode on port ${port}`
+  );
+});
